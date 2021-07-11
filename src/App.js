3c506dfb6956f82884e6ee0fb4a424fb78bf8b0c
federicomoscardo <federico.moscardo@gmail.com>
2021-07-11 17:01:40 -0300

    added CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react'
 import './App.css';
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Col, Row} from 'react-bootstrap'
 import Display from './components/Display'
 import Buttons from './components/Buttons'
-import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Container fluid>
-  
-          <Display />
-  
-          <Buttons />
-          
+        <Container className="calculator py-2" fluid>
+          <Row>
+            <Col>
+              <Display />
+              <Buttons />
+            </Col>
+          </Row>
         </Container>
-      </div>
     )
   }
 }
