6a84f8f5b579102c450ee3496d652f2a7ed18bc6
federicomoscardo <federico.moscardo@gmail.com>
2021-07-11 16:59:16 -0300

    finished functions, added the function 'clear' and CSS

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,8 +7,8 @@ class Button extends Component {
     actionOnClick = () => {
         switch(this.props.type){
             case 'number':
-                if (this.props.display == 0 || this.props.display == this.props.operand) {
-                    this.props.dispatch({type: "ERASE_DISPLAY"})
+                if (!this.props.addingNumber) {
+                    this.props.dispatch({type: "ERASE_DISPLAY", payload: false})
                 }
                 this.props.dispatch({type: "RECEIVE_NUMBER", payload: this.props.text})
                 break
@@ -21,21 +21,43 @@ class Button extends Component {
             case 'operator':
                 if (this.props.result == "") {
                     this.props.dispatch({type: "FIRST_VALUE", payload: this.props.text})
-                    this.props.dispatch({type: "SHOW_RESULT"})
                     this.props.dispatch({type: "SHOW_OPERATION", payload: this.props.text})
+                    this.props.dispatch({type: "SHOW_RESULT"})
                 } else {
                     this.props.dispatch({type: "SHOW_OPERATION", payload: this.props.text})
-                    //this.props.dispatch({type: "ERASE_DISPLAY"})
                     this.props.dispatch({type: "DO_OPERATION", payload: this.props.text})
                     this.props.dispatch({type: "SHOW_RESULT"})
+                }
+                break
+            case 'clear':
+                if (this.props.display != 0) {
+                    this.props.dispatch({type: "ERASE_DISPLAY", payload: true})
+                } else {
+                    this.props.dispatch({type: "ERASE_ALL"})
                 }
                 break
         }
     }
 
+    styleButton() {
+        switch(this.props.type){
+            case 'number':
+                return "btn btn-secondary"
+            case 'dot':
+                return "btn btn-secondary"
+            case 'operator':
+                if (this.props.text == "="){
+                    return "btn btn-success"
+                }
+                return "btn btn-primary"
+            case 'clear':
+                return "btn btn-danger"
+        }
+    }
+
     render() {
         return (
-            <button onClick={this.actionOnClick}>
+            <button onClick={this.actionOnClick} className={this.styleButton()}>
                 {this.props.text}
             </button>
         )
@@ -46,7 +68,7 @@ function mapStateToProps(state) {
     return {
         display: state.display,
         result: state.result,
-        operand: state.operand
+        addingNumber: state.addingNumber
     }
 }
 
